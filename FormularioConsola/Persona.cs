6384c87ec70e8dc05c71ac1e2c79f5a3537e4bf3
Leosoft <leosoft3031@gmail.com>
2021-04-21 10:41:56 -0300

    Agregar archivos de proyecto.

---
+++ FormularioConsola/Persona.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularioConsola
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string email;
+        private double monto;
+        
+
+        public Persona()
+        {
+
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public string getApellido()
+        {
+            return apellido;
+        }
+
+        public void setApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+
+        public int getEdad()
+        {
+            return edad;
+        }
+
+        public void setEdad(int edad)
+        {
+            this.edad = edad;
+        }
+
+        public string getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public double getMonto()
+        {
+            return monto;
+        }
+
+        public void setMonto(double monto)
+        {
+            this.monto = monto;
+        }
+    }
+}
---
+++ FormularioConsola/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FormularioConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona per = new Persona();
+            Console.Write("Ingresa tu nombre: ");
+            per.setNombre(Console.ReadLine());
+            Console.Write("Ingresa tu apellido: ");
+            per.setApellido(Console.ReadLine());
+            Console.Write("Ingresa tu edad: ");
+            per.setEdad(Int16.Parse(Console.ReadLine()));
+            Console.Write("Ingresa tu email: ");
+            per.setEmail(Console.ReadLine());
+            Console.Write("Ingresa el monto que desees: ");
+            per.setMonto(Double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Nombre: " + per.getNombre() + "  \nApellido: " + per.getApellido() +
+                " \nEdad: " + per.getEdad() + " \nEmail: " + per.getEmail() + " \nMonto: "
+                + per.getMonto());
+            
+            
+        }
+    }
+}
